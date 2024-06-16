c9bb348687559200e1c9606d6136966884aff52e
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-16 22:11:20 +0300

    footer page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <p className='text'>&copy; E-commerce. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
